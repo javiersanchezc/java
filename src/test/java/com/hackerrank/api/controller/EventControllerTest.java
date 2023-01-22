4d82980cdc586bdb10fda2b47f23337281a8e7c8
Admin <admin.push@hackerrank.workspace>
2023-01-22 16:52:31 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/test/java/com/hackerrank/api/controller/EventControllerTest.java
+++ src/test/java/com/hackerrank/api/controller/EventControllerTest.java
@@ -2,17 +2,22 @@ package com.hackerrank.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hackerrank.api.model.Event;
-import com.hackerrank.api.repository.EventRepository;
-import static org.hamcrest.Matchers.greaterThan;
 
+import com.hackerrank.api.service.EventService;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +25,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 
 @ExtendWith(SpringExtension.class)
@@ -30,8 +37,12 @@ class EventControllerTest {
   ObjectMapper om = new ObjectMapper();
   @Autowired
   private MockMvc mockMvc;
-  @Autowired
-  private EventRepository repository;
+
+  @Mock
+  private EventService eventService;
+
+  @InjectMocks
+  private EventController eventController;
 
   @Test
   public void testCreation() throws Exception {
@@ -47,6 +58,12 @@ class EventControllerTest {
   }
 
   
+  @Test
+  public void testTop3By_InvalidSortBy() throws Exception {
+      String sortBy = "invalid";
+      ResponseEntity<List<Event>> response = eventController.top3By(sortBy);
+      assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+  }
   
   
 }
